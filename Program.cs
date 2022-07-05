abce7f053fe40ac14a169e74c8999a852d57da6f
Ezh+Wine <valvic90@gmail.com>
2022-07-05 15:03:39 +0300

    fibonacci task sompleted

--- Program.cs
+++ Program.cs
@@ -1,74 +1,78 @@
-﻿// // Напишите программу, которая будет преобразовывать десятичное число в двоичное.
-// // 45 -> 101101
-// // 3 -> 11
-// // 2 -> 10
-
-
-// int GetDecimal(5)
-// {
-// while (decimalNum > 0)
-//     {   
-//         int result = 0;
-//         if (decimalNum % 2 == 1)
-//         {
-
-//             result = result * 10 + 1;
-//         }
-//         else
-//         {
-//             result = result * 10;
-//         }
-//         decimalNum = decimalNum / 2;
-//     }
-//     return result;
-// }
-
-// Console.WriteLine(result);
-
-
+﻿
 // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
 
-int[] CreateArray(int size)
-{
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        Console.Write("Input " + i + " number: ");
-        array[i] = Convert.ToInt32(Console.ReadLine());
-    }
-    return array;
-}
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.Write("Input " + i + " number: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
 
-void ShowArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-}
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+// }
 
-int CountEvenElements(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] > 0)
-            count++;
-    }
-    return count;
-}
+// int CountEvenElements(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//             count++;
+//     }
+//     return count;
+// }
 
-Console.WriteLine("How many numbers you want to input?");
-int quantity = Convert.ToInt32(Console.ReadLine());
-int[] newArray = CreateArray(quantity);
-ShowArray(newArray);
-Console.WriteLine();
-Console.WriteLine($"WOW! {CountEvenElements(newArray)} numbers are positive");
+// Console.WriteLine("How many numbers you want to input?");
+// int quantity = Convert.ToInt32(Console.ReadLine());
+// int[] newArray = CreateArray(quantity);
+// ShowArray(newArray);
+// Console.WriteLine();
+// Console.WriteLine($"WOW! {CountEvenElements(newArray)} numbers are positive");
 
 
 
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+// void Fibonacci(int size)
+// {
+//     int[] array = new int[size];
+//     array[0] = 0;
+//     array[1] = 1;
+//     for (int i = 2; i < array.Length; i++)
+//     {
+//         array[i] = array[i - 1] + array[i - 2];
+//         Console.Write(array[i] + " ");
+//     }
+// }
+
+// Console.WriteLine("Input number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("0 1 ");
+// Fibonacci(number);
+
+// какое-то читерское решение получилось
+
+
+
+
+
+
+
+
 
